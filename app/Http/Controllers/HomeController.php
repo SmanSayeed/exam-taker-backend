791e7eb1f135395e6d29110dc1d487fe03bf81fc
saadman.sayeed <saadman.sayeed@bjitgroup.com>
2024-11-13 15:38:52 +0600

    Working with home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function welcome(){
-        return view('welcome');
+        return view('pages.home');
     }
 }
