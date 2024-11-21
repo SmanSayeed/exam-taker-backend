d82ddd2079fd992471fac713801af4e393d8c72e
Mohiul Islam <mohiulislam900@gmail.com>
2024-11-21 13:12:36 +0600

    navigation indication added

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,41 +1,37 @@
-  <!-- Footer Nav -->
-  <div class="footer-nav-area" id="footerNav">
+<!-- Footer Nav -->
+<div class="footer-nav-area" id="footerNav">
     <div class="container px-0">
-      <!-- Footer Content -->
-      <div class="footer-nav position-relative">
-        <ul class="h-100 d-flex align-items-center justify-content-between ps-0">
-          <li class="active">
-            <a href={{route('home')}}>
-              <i class="bi bi-house"></i>
-              <span>Home</span>
-            </a>
-          </li>
+        <!-- Footer Content -->
+        <div class="footer-nav position-relative">
+            <ul class="h-100 d-flex align-items-center justify-content-between ps-0">
+                <li class="{{ request()->routeIs('home') ? 'active' : '' }}">
+                    <a href="{{ route('home') }}">
+                        <i class="bi bi-house"></i>
+                        <span>Home</span>
+                    </a>
+                </li>
 
+                <li class="{{ request()->is('question-bank*') ? 'active' : '' }}">
+                    <a href="elements.html">
+                        <i class="fa-solid fa-list-check"></i>
+                        <span>Question Bank</span>
+                    </a>
+                </li>
 
+                <li class="{{ request()->is('practice*') ? 'active' : '' }}">
+                    <a href="#">
+                        <i class="fa-regular fa-pen-to-square"></i>
+                        <span>Practice</span>
+                    </a>
+                </li>
 
-          <li>
-            <a href="elements.html">
-                <i class="fa-solid fa-list-check"></i>
-              <span>Question Bank</span>
-            </a>
-          </li>
-
-          <li>
-            <a href="#">
-          <i class="fa-regular fa-pen-to-square"></i>
-              <span>Practice</span>
-            </a>
-          </li>
-
-          <li>
-            <a href="#">
-                <i class="fa-solid fa-person-chalkboard"></i>
-              <span>Course</span>
-            </a>
-          </li>
-
-
-        </ul>
-      </div>
+                <li class="{{ request()->is('course*') ? 'active' : '' }}">
+                    <a href="#">
+                        <i class="fa-solid fa-person-chalkboard"></i>
+                        <span>Course</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
+</div>
