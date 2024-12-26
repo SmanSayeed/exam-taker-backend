b285737ff6d56aba628e92a712ed702e8285f138
Mohiul Islam <mohiulislam900@gmail.com>
2024-12-26 15:49:02 +0600

    status issue fixed

--- app/Http/Resources/AdditionalPackageCategoryResource.php
+++ app/Http/Resources/AdditionalPackageCategoryResource.php
@@ -18,7 +18,7 @@ class AdditionalPackageCategoryResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
-            'status' => $this->status,
+            'is_active' => $this->is_active,
             'created_at' => $this->created_at->toDateTimeString(),
             'updated_at' => $this->updated_at->toDateTimeString(),
         ];
